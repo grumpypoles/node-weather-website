7b1053b16fd2ecc144f1daecf75e29779c774eb8
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2021-12-27 17:21:47 +1300

    Heroku Updates

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = proces.env.PORT || 3000
 
 //Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -123,6 +124,6 @@ app.get('*', (req, res) =>{
 //app.com/help
 //app.com/about
 
-app.listen(3000, () =>{
-  console.log('Server is up on port 3000')
+app.listen(port, () =>{
+  console.log(`Server is up on port ${port}`)
 })
